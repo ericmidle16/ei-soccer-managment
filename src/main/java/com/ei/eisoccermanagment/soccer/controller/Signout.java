02e601fec82db66966bd27b923fe24b80e1d2122
Eric Idle <ericmidle16@gmail.com>
2025-02-28 21:16:01 -0600

    Users able to sign out

--- src/main/java/com/ei/eisoccermanagment/soccer/controller/Signout.java
+++ src/main/java/com/ei/eisoccermanagment/soccer/controller/Signout.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-@WebServlet("/dat-signout")
+@WebServlet("/signout")
 public class Signout extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
